2e9a3271c62584e69a11d7fa4f6b6afce22fabda
diosamuel <koding12sam@gmail.com>
2024-09-28 08:51:47 +0700

    update: tentang page content

--- src/pages/Tentang.tsx
+++ src/pages/Tentang.tsx
@@ -23,13 +23,13 @@ const AboutPage: React.FC = () => {
         {/* Description Section */}
         <div className="text-gray-700 text-base leading-relaxed max-w-4xl mx-auto mb-12">
           <p className="mb-4">
-            Peningkatan komersialisasi inovasi membutuhkan peningkatan knowledge flow baik ke dalam maupun ke luar IPB melalui jejaring dan kemitraan strategis. Peningkatan knowledge flow ke dalam IPB diperlukan untuk mendapatkan informasi tentang inovasi yang dibutuhkan oleh industri, sedangkan peningkatan knowledge flow ke luar IPB dilakukan untuk mempromosikan komersialisasi inovasi IPB kepada industri. Terutama industri dalam negeri agar dapat memanfaatkan inovasi. Dengan lancarnya flow of knowledge tersebut maka agenda riset IPB akan dapat difokuskan secara lebih tajam, sesuai kebutuhan industri dan strategi inovasi dalam upaya mengatasi masalah-masalah perekonomian, sekaligus meningkatkan income generating IPB.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium facilis sapiente dolorem commodi dolor adipisci cupiditate voluptates tenetur ipsa expedita eos exercitationem, saepe earum officia. Vero sapiente repellendus repudiandae est.
           </p>
           <p className="mb-4">
-            IPB Innovation dirancang sebagai alat untuk mempromosikan/menawari inovasi yang dihasilkan oleh sivitas akademika IPB kepada masyarakat industri dan calon penggunanya. Inovasi IPB ini didasarkan pada kegiatan riset yang mengedepankan kepakaran dan kompetensi riset dari para inovator IPB. Inovator juga berfungsi sebagai role model secara profesional antar inovator dengan calon pengguna teknologi atau untuk mitra bisnis IPB. Inovasi IPB menawarkan fitur-fitur plotifatif yang lengkap, mulai dari hasil riset dalam skala laboratorium, prototype, paten, lisensi, hingga produk yang siap dipasarkan.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium facilis sapiente dolorem commodi dolor adipisci cupiditate voluptates tenetur ipsa expedita eos exercitationem, saepe earum officia. Vero sapiente repellendus repudiandae est.
           </p>
           <p className="mb-4">
-            IPB Innovation juga menjadi alat diseminasi secara terbuka mengenai inovasi yang dihasilkan oleh inovator, termasuk hasil dan kesiapan teknologi.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium facilis sapiente dolorem commodi dolor adipisci cupiditate voluptates tenetur ipsa expedita eos exercitationem, saepe earum officia. Vero sapiente repellendus repudiandae est.
           </p>
         </div>
 

449a7a3c2d220114ed0fb7a15ec189e63faaf2e7
diosamuel <koding12sam@gmail.com>
2024-09-28 13:14:30 +0700

    feat: Add Innovator Dashboard Path

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ListInovasi from './pages/ListInovasi';
-import Login from './pages/Login';
-import Register from './pages/Register';
 import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
-import Tentang from './pages/Tentang';
-import DetailInovasi from './pages/DetailInovasi';
 import React from 'react';
 import App from './App';
 import './index.css'; 
 import './App.css';
 
-import KategoriesAdmin from './pages/admin/categories';
+import ListInovasi from '@/pages/ListInovasi';
+import Login from '@/pages/Login';
+import Register from '@/pages/Register';
+import Tentang from '@/pages/Tentang';
+import DetailInovasi from '@/pages/DetailInovasi';
+import KategoriesAdmin from '@/pages/admin/categories';
 import Index from '@/pages/admin/index';
-import InovationAdmin from './pages/admin/inovations';
-
-import ForgotPassword from './pages/ForgotPassword';
-
+import InovationAdmin from '@/pages/admin/inovations';
+import ForgotPassword from '@/pages/ForgotPassword';
+import InnovatorListInovasi from '@/pages/innovator/innovations/ListInovasi';
+import InnovatorTambahInovasi from '@/pages/innovator/innovations/TambahInovasi';
+import InnovatorWishlistInovasi from '@/pages/innovator/wishlist/WishlistInovasi';
+import Profil from '@/pages/Profile';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -33,6 +35,11 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
           <Route path="/register" element={<Register />} />
           <Route path="/inovasi/:id" element={<DetailInovasi />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+
+          <Route path="/innovator/profil" element={<Profil/>}/>
+          <Route path="/innovator/list-inovasi" element={<InnovatorListInovasi/>}/>
+          <Route path="/innovator/tambah-inovasi" element={<InnovatorTambahInovasi/>}/>
+          <Route path="/innovator/wishlist-inovasi" element={<InnovatorWishlistInovasi/>}/>
         </Routes>
       </Router>
     </ChakraProvider>

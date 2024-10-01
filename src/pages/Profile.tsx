aed0d0b5ef0f56485b7c03faf4e871070d252a13
diosamuel <koding12sam@gmail.com>
2024-10-01 22:14:16 +0700

    fix: Update PUT Profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -13,6 +13,8 @@ import {
   Grid,
   GridItem,
   useToast,
+  FormErrorMessage,
+  Link,
 } from "@chakra-ui/react";
 import { useFormik } from "formik";
 import { IconPhotoPlus } from "@tabler/icons-react";
@@ -20,38 +22,37 @@ import * as Yup from "yup";
 import AuthApiService from "@/services/apiServices/auth.api.service";
 import Layout from "@/components/innovator/layoutInnovator/LayoutInnovator";
 import { FAKULTAS } from "@/lib/constants/fakultas.constans";
-import { PRODI } from '@/lib/constants/prodi.constans'
+import { PRODI } from "@/lib/constants/prodi.constans";
+import { put } from "@/hooks/useSubmit";
 
-// Validation schema using Yup
 const validationSchema = Yup.object({
-  name: Yup.string().required("Nama is required"),
-  email: Yup.string()
-    .email("Alamat email tidak valid")
-    .required("Email is required"),
-  phone: Yup.string()
+  fullname: Yup.string().required("Nama is required"),
+  phonenumber: Yup.string()
     .matches(/^[0-9]+$/, "Hanya angka yang diperbolehkan")
     .min(10, "Nomor telepon minimal 10 digit")
     .max(15, "Nomor telepon maksimal 15 digit")
     .required("No Telepon is required"),
-  dob: Yup.date()
+  dateOfBirth: Yup.date()
     .max(new Date(), "Tanggal Lahir tidak boleh di masa depan")
     .required("Tanggal Lahir is required"),
   gender: Yup.string().required("Jenis Kelamin is required"),
   fakultas: Yup.string().required("Fakultas is required"),
   prodi: Yup.string().required("Program Studi is required"),
 });
+
 const EditProfile = () => {
-  console.log(PRODI,PRODI[FAKULTAS.FS])
   const [profileData, setProfileData] = useState(null);
+  const [tempProfileData, setTempProfileData] = useState(null);
   const [profilePic, setProfilePic] = useState("");
   const toast = useToast();
 
   const getProfile = async () => {
     try {
       const response = await AuthApiService.me();
-      console.log(response.data.data);
       setProfileData(response.data.data);
       setProfilePic(response.data.data.profile);
+
+      setTempProfileData(response.data.data);
     } catch (error) {
       console.error("Failed to fetch profile:", error);
     }
@@ -63,10 +64,10 @@ const EditProfile = () => {
 
   const formik = useFormik({
     initialValues: {
-      name: profileData?.fullname || "",
+      fullname: profileData?.fullname || "",
       email: profileData?.email || "",
-      phone: profileData?.phonenumber || "",
-      dob: profileData?.dateOfBirth || "",
+      phonenumber: profileData?.phonenumber || "",
+      dateOfBirth: profileData?.dateOfBirth || "",
       gender: profileData?.gender || "1",
       fakultas: profileData?.inovator.fakultas || "",
       prodi: profileData?.inovator.prodi || "",
@@ -75,29 +76,25 @@ const EditProfile = () => {
     enableReinitialize: true,
     onSubmit: async (values, { setSubmitting }) => {
       try {
-        const formData = new FormData();
-        formData.append("name", values.name);
-        formData.append("email", values.email);
-        formData.append("phone", values.phone);
-        formData.append("dob", values.dob);
-        formData.append("gender", values.gender);
-        formData.append("fakultas", values.fakultas);
-        formData.append("prodi", values.prodi);
-        if (profilePic) {
-          const response = await fetch(profilePic);
-          const blob = await response.blob();
-          formData.append("profilePic", blob, "profilePic.jpg");
-        }
+        let constructForm = {
+          ...values,
+          // "inovator.fakultas":values.fakultas,
+          // "inovator.prodi":values.prodi
+        };
+        // delete constructForm.fakultas;
+        // delete constructForm.prodi;
 
-        console.log(values);
-        // await AuthApiService.updateProfile(formData);
+        await put({
+          url: `/api/v1/users/${profileData?._id}`,
+          data: constructForm,
+        });
         toast({
           title: "Profil diperbarui.",
           description: "Informasi profil Anda telah diperbarui.",
           status: "success",
           duration: 3000,
           isClosable: true,
-          position: "top-right"
+          position: "top-right",
         });
       } catch (error) {
         console.error("Failed to update profile:", error);
@@ -155,13 +152,14 @@ const EditProfile = () => {
                   >
                     Ganti Foto Profil
                   </Button>
-                  <Box className="p-5">
-                    <p className="font-medium">
-                      Anda Mendaftar Sebagai{" "}
-                      <Badge fontSize={"lg"} colorScheme="green">
-                        {profileData.role.toUpperCase()}
-                      </Badge>
-                    </p>
+
+                  <Box className="p-5 border flex flex-col justify-center items-center gap-4">
+                    <h1 className="text-lg font-bold">
+                      Ingin mengubah password?
+                    </h1>
+                    <Link href="/forgot-password">
+                      <Button colorScheme="blue">Ubah Password</Button>
+                    </Link>
                   </Box>
                 </VStack>
               </FormControl>
@@ -170,15 +168,13 @@ const EditProfile = () => {
             {/* Column 2: Form Fields */}
             <GridItem>
               <VStack spacing={4} align="stretch">
-                <Box>
+                <Box className="w-full">
                   {profileData.inovator.status === "pending" && (
                     <Box className="rounded p-3 bg-orange-600 text-white font-medium space-y-2">
                       <Badge colorScheme="orange" fontSize={"sm"}>
                         Pending
                       </Badge>
-                      <p className="font-medium">
-                        Menunggu Konfirmasi Admin
-                      </p>
+                      <p className="font-medium">Menunggu Konfirmasi Admin</p>
                     </Box>
                   )}
                   {profileData.inovator.status === "approved" && (
@@ -197,46 +193,75 @@ const EditProfile = () => {
                     </Box>
                   )}
                 </Box>
-
-                {/* Form fields */}
-                <FormControl>
+                {/* Nama */}
+                <FormControl
+                  isInvalid={formik.touched.fullname && formik.errors.fullname}
+                >
                   <FormLabel>Nama</FormLabel>
                   <Input
-                    {...formik.getFieldProps('name')}
+                    {...formik.getFieldProps("fullname")}
                     placeholder="Masukkan Nama"
+                  />
+                  <FormErrorMessage>{formik.errors.fullname}</FormErrorMessage>
+                </FormControl>
+
+                {/* Daftar Sebagai */}
+                <FormControl>
+                  <FormLabel>Terdaftar Sebagai</FormLabel>
+                  <Input
+                    disabled={true}
+                    value={profileData.role.toUpperCase()}
                   />
                 </FormControl>
 
+                {/* Email */}
                 <FormControl>
                   <FormLabel>Email</FormLabel>
                   <Input
-                    {...formik.getFieldProps('email')}
+                    {...formik.getFieldProps("email")}
                     type="email"
-                    placeholder="Masukkan Email"
+                    disabled
                   />
                 </FormControl>
 
-                <FormControl>
+                {/* No Telepon */}
+                <FormControl
+                  isInvalid={
+                    formik.touched.phonenumber && formik.errors.phonenumber
+                  }
+                >
                   <FormLabel>No Telepon</FormLabel>
                   <Input
-                    {...formik.getFieldProps('phone')}
+                    {...formik.getFieldProps("phonenumber")}
                     placeholder="Masukkan No Telepon"
                   />
+                  <FormErrorMessage>
+                    {formik.errors.phonenumber}
+                  </FormErrorMessage>
                 </FormControl>
 
-                <FormControl>
+                {/* Tanggal Lahir */}
+                <FormControl
+                  isInvalid={
+                    formik.touched.dateOfBirth && formik.errors.dateOfBirth
+                  }
+                >
                   <FormLabel>Tanggal Lahir</FormLabel>
-                  <Input
-                    {...formik.getFieldProps('dob')}
-                    type="date"
-                  />
+                  <Input {...formik.getFieldProps("dateOfBirth")} type="date" />
+                  <FormErrorMessage>
+                    {formik.errors.dateOfBirth}
+                  </FormErrorMessage>
                 </FormControl>
 
-                <FormControl as="fieldset">
+                {/* Jenis Kelamin */}
+                <FormControl
+                  as="fieldset"
+                  isInvalid={formik.touched.gender && formik.errors.gender}
+                >
                   <FormLabel as="legend">Jenis Kelamin</FormLabel>
                   <RadioGroup
-                    {...formik.getFieldProps('gender')}
-                    onChange={(val) => formik.setFieldValue('gender', val)}
+                    {...formik.getFieldProps("gender")}
+                    onChange={(val) => formik.setFieldValue("gender", val)}
                     value={formik.values.gender}
                   >
                     <VStack spacing="8px" align={"left"}>
@@ -244,44 +269,82 @@ const EditProfile = () => {
                       <Radio value="Perempuan">Perempuan</Radio>
                     </VStack>
                   </RadioGroup>
+                  <FormErrorMessage>{formik.errors.gender}</FormErrorMessage>
                 </FormControl>
 
-                <FormControl>
-                  <FormLabel>Fakultas</FormLabel>
-                  <Select
-                    {...formik.getFieldProps('fakultas')}
-                    placeholder="Pilih Fakultas"
-                  >
-                    <option value="Fakultas Sains">Fakultas Sains</option>
-                    <option value="Fakultas Teknologi Infrastruktur & Kewilayahan">
-                      Fakultas Teknologi Infrastruktur & Kewilayahan
-                    </option>
-                    <option value="Fakultas Teknologi Industri">
-                      Fakultas Teknologi Industri
-                    </option>
-                  </Select>
-                </FormControl>
+                {/* Fakultas */}
+                {profileData.role == "innovator" && (
+                  <>
+                    <FormControl
+                      isInvalid={
+                        formik.touched.fakultas && formik.errors.fakultas
+                      }
+                    >
+                      <FormLabel>Fakultas</FormLabel>
+                      <Select
+                        {...formik.getFieldProps("fakultas")}
+                        onChange={(e) => {
+                          setTempProfileData((prev) => ({
+                            ...prev,
+                            inovator: {
+                              ...prev.inovator,
+                              fakultas: e.target.value,
+                            },
+                          }));
+                          formik.handleChange(e);
+                        }}
+                        placeholder="Pilih Fakultas"
+                      >
+                        <option value="Fakultas Sains">Fakultas Sains</option>
+                        <option value="Fakultas Teknologi Infrastruktur & Kewilayahan">
+                          Fakultas Teknologi Infrastruktur & Kewilayahan
+                        </option>
+                        <option value="Fakultas Teknologi Industri">
+                          Fakultas Teknologi Industri
+                        </option>
+                      </Select>
+                      <FormErrorMessage>
+                        {formik.errors.fakultas}
+                      </FormErrorMessage>
+                    </FormControl>
 
-                <FormControl>
-                  <FormLabel>Program Studi</FormLabel>
-                  <Select
-                    {...formik.getFieldProps('prodi')}
-                    placeholder="Pilih Program Studi"
-                  >
-                    {profileData.inovator.fakultas === "Fakultas Teknologi Industri" &&
-                      Object.keys(PRODI[FAKULTAS.FTI]).map((prodi) => (
-                        <option key={prodi.nama} value={prodi}>{PRODI[FAKULTAS.FTI][prodi]}</option>
-                      ))}
-                    {profileData.inovator.fakultas === "Fakultas Teknologi Infrastruktur & Kewilayahan" &&
-                      Object.keys(PRODI[FAKULTAS.FTIK]).map((prodi) => (
-                        <option key={prodi.nama} value={prodi.nama}>{PRODI[FAKULTAS.FTIK][prodi]}</option>
-                      ))}
-                    {profileData.inovator.fakultas === "Fakultas Sains" &&
-                      Object.keys(PRODI[FAKULTAS.FS]).map((prodi) => (
-                        <option key={prodi.nama} value={prodi.nama}>{PRODI[FAKULTAS.FS][prodi]}</option>
-                      ))}
-                  </Select>
-                </FormControl>
+                    {/* Program Studi */}
+                    <FormControl
+                      isInvalid={formik.touched.prodi && formik.errors.prodi}
+                    >
+                      <FormLabel>Program Studi</FormLabel>
+                      <Select
+                        {...formik.getFieldProps("prodi")}
+                        placeholder="Pilih Program Studi"
+                      >
+                        {tempProfileData.inovator.fakultas ===
+                          "Fakultas Teknologi Industri" &&
+                          Object.keys(PRODI[FAKULTAS.FTI]).map((prodi, key) => (
+                            <option key={key} value={prodi}>
+                              {PRODI[FAKULTAS.FTI][prodi]}
+                            </option>
+                          ))}
+                        {tempProfileData.inovator.fakultas ===
+                          "Fakultas Teknologi Infrastruktur & Kewilayahan" &&
+                          Object.keys(PRODI[FAKULTAS.FTIK]).map(
+                            (prodi, key) => (
+                              <option key={key} value={prodi.nama}>
+                                {PRODI[FAKULTAS.FTIK][prodi]}
+                              </option>
+                            )
+                          )}
+                        {tempProfileData.inovator.fakultas ===
+                          "Fakultas Sains" &&
+                          Object.keys(PRODI[FAKULTAS.FS]).map((prodi, key) => (
+                            <option key={key} value={prodi.nama}>
+                              {PRODI[FAKULTAS.FS][prodi]}
+                            </option>
+                          ))}
+                      </Select>
+                      <FormErrorMessage>{formik.errors.prodi}</FormErrorMessage>
+                    </FormControl>
+                  </>
+                )}
 
                 <Button
                   type="submit"
